e2ea7b9b248a0f4e51ca003a857863525521cd05
avosalmon <avosalmon@gmail.com>
2019-07-03 21:57:54 +0900

    add webpack-manifest-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -23,7 +24,8 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Webpack caching test'
-    })
+    }),
+    new ManifestPlugin()
   ],
   optimization: {
     moduleIds: 'hashed',
