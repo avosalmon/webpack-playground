1ba9afa8a58a3f6a9954df7a23af1c045749d13e
avosalmon <avosalmon@gmail.com>
2019-07-15 15:23:09 +0900

    remove html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 
 module.exports = {
@@ -36,9 +35,6 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      title: 'Webpack caching test'
-    }),
     new ManifestPlugin()
   ],
   optimization: {
