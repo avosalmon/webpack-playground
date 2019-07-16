e75b64ac49417a962ccebb416807b5c02ff9d823
avosalmon <avosalmon@gmail.com>
2019-07-17 07:45:15 +0900

    Apply CSS extraction only for production

--- webpack.config.js
+++ webpack.config.js
@@ -6,76 +6,81 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const { VueLoaderPlugin } = require('vue-loader');
 
-module.exports = {
-  entry: {
-    app: [
-      './src/index.js',
-      './src/style.scss'
-    ]
-  },
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        use: [
-          {
-            // Loads ES2015+ code and transpiles to ES5 using Babel
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                '@babel/preset-env'
-              ]
-            }
-          }
-        ]
-      },
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production';
 
-      // This will apply to both plain `.scss` files
-      // AND `<style lang="scss">` blocks in `.vue` files
-      {
-        test: /\.scss$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new ManifestPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash].css'
-    }),
-    new VueLoaderPlugin()
-  ],
-  optimization: {
-    moduleIds: 'hashed',
-    runtimeChunk: 'single',
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
+  return {
+    entry: {
+      app: [
+        './src/index.js',
+        './src/style.scss'
+      ]
     },
-    minimizer: [
-      // Webpack minimize JS by default
-      // but we have to specify minimizer if we overwrite minimizer settings.
-      new UglifyJsPlugin,
-      new OptimizeCssAssetsPlugin
-    ]
-  }
+    output: {
+      filename: '[name].[contenthash].js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+            {
+              // Loads ES2015+ code and transpiles to ES5 using Babel
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  '@babel/preset-env'
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+
+        // This will apply to both plain `.scss` files
+        // AND `<style lang="scss">` blocks in `.vue` files
+        {
+          test: /\.scss$/,
+          use: [
+            // Only apply CSS extraction for production so that
+            // we get CSS hot reload during development.
+            isProd ? MiniCssExtractPlugin.loader : 'vue-style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new ManifestPlugin(),
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css'
+      }),
+      new VueLoaderPlugin()
+    ],
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      },
+      minimizer: [
+        // Webpack minimize JS by default
+        // but we have to specify minimizer if we overwrite minimizer settings.
+        new UglifyJsPlugin,
+        new OptimizeCssAssetsPlugin
+      ]
+    }
+  };
 };
